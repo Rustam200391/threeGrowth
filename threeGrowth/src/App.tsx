2d1fb63eeef44950008785356639e9ea17823d29
Rustam Guseinov <rg20031991.com>
2024-02-09 08:03:23 +0300

    intermediate commit

--- threeGrowth/src/App.tsx
+++ threeGrowth/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+      <h1>ThreeGrowth app</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
